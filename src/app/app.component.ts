8c57adfea213713b2671f06a6fe84d03f56fee54
jsongo <jsongo@qq.com>
2017-09-25 12:26:14 +0800

    update to use viewContainerRef

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,20 +19,6 @@ export class AppComponent implements AfterViewInit {
         if (!this.tplRef) {
             return;
         }
-        // 取到EmbeddedViewRef对象
-        const viewRef = this.tplRef.createEmbeddedView(null);
-        // 拿到rootNodes节点内容
-        const nodes = viewRef.rootNodes;
-        // 得到在页面中模板
-        const tplDom = this.tplRef.elementRef.nativeElement;
-        // 遍历所有的节点一个个插入
-        nodes.forEach((node: any) => {
-            // 插入到模板的前面
-            tplDom.parentNode.insertBefore(node, tplDom);
-        });
-        // 接入Angular内核的变化检测中
-        viewRef.detectChanges();
-
         // 或者直接使用下面的方法
         this.viewContainer.createEmbeddedView(this.tplRef);
     }
